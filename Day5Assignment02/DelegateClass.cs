7490242da24aa962db6f378a149a12eb5e2c33ef
Nexwave <Nexwave@DESKTOP-TDF6V7V>
2018-08-06 08:52:36 +0530

    Add project files.

---
+++ Day5Assignment02/DelegateClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignment02
+{
+    class DelegateClass
+    {
+        public void printNumber(int number)
+        {
+            Console.WriteLine("{0:N}", number);
+        }
+        public void printMoney(int number)
+        {
+            Console.WriteLine("{0:C}", number);
+        }
+        public void printHexadecimal(int number)
+        {
+            Console.WriteLine("{0:X}", number);
+        }
+
+    }
+}
---
+++ Day5Assignment02/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignment02
+{
+    public delegate void mydel(int num);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateClass dc = new DelegateClass();
+
+            //int number = 2349465;
+            mydel del = dc.printNumber;
+
+            del += dc.printMoney;
+            del += dc.printHexadecimal;
+
+            del(10000000);
+          
+           
+        }
+    }
+}
